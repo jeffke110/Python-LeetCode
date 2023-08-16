4afc2bb7e3f47b505fcd896c2a3da91b3fa0fa2a
jeffke110 <jeffke@vt.edu>
2023-08-16 16:30:32 -0400

    longest common prefix

--- Array/longest-common-prefix.py
+++ Array/longest-common-prefix.py
@@ -3,4 +3,25 @@ from typing import List
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        print("hello")
+
+        if len(strs) < 1:
+            return ""
+        cmpstr : str = strs[0]
+
+        # fish fif
+        for index in range(1, len(strs)):
+
+            element = strs[index]
+            charIndex = 0
+            while(charIndex < len(cmpstr) and charIndex < len(element) and 
+                    cmpstr[charIndex] == element[charIndex] ):
+                charIndex += 1
+
+
+            if charIndex < len(cmpstr):
+                cmpstr = cmpstr[:charIndex]
+            elif charIndex < len(element):
+                cmpstr = element[:charIndex]
+
+        return cmpstr
+            

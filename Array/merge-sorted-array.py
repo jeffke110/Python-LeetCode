84fea6a45f1dfa4855aa5c5f53e6dd83e15bd230
jeffke110 <jeffke@vt.edu>
2023-08-21 10:59:51 -0400

    merge sorted array

---
+++ Array/merge-sorted-array.py
@@ -0,0 +1,11 @@
+
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        for index in range(m, len(nums1)):
+            nums1.pop()
+        nums1.extend(nums2)
+        nums1.sort()
+
+
